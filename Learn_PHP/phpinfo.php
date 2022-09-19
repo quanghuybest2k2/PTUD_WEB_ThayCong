ba956e806f8fe60f8b6f60a57d701033a7212577
Doan Quang Huy <quanghuybest@gmail.com>
2022-09-19 21:19:37 +0700

    cap nhat file phpinfo

--- Learn_PHP/phpinfo.php
+++ Learn_PHP/phpinfo.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP info</title>
-</head>
-
-<body>
-    <?php phpinfo() ?>
     <?php
-    $x = 6;
-    $y = 7;
-    $sum = $x + $y;
-    echo $sum;
+    phpinfo();
+    // $x = 6;
+    // $y = 7;
+    // $sum = $x + $y;
+    // echo $sum;
     ?>
-</body>
-
-</html>
